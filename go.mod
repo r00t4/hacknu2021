82d70f92309d2560cb91ab650e03c8a5fda00a5a
Kazbek Borash <kazbek@sprintsquads.com>
2021-03-21 04:44:44 +0600

    fix: rooms

--- go.mod
+++ go.mod
@@ -4,6 +4,10 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.4.2
 	github.com/pion/rtcp v1.2.6
+	github.com/pion/rtp v1.6.2
+	github.com/pion/webrtc/v2 v2.2.26
 	github.com/pion/webrtc/v3 v3.0.16
 )
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/r00t4/hacknu2021/lib"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,18 +16,66 @@ func main() {
 	//handlers := lib.Handlers{
 	//	Service: service,
 	//}
-	b := make(chan string, 3)
-	ans := make(chan string, 3)
-	http.HandleFunc("/establish-video-sdp", func(w http.ResponseWriter, r *http.Request) {
+	rooms := lib.NewRooms()
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/stats", func(w http.ResponseWriter, r *http.Request) {
+		bytes, err := json.Marshal(rooms.GetStats())
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), 500)
+		}
+		w.Write(bytes)
+	}).Methods("GET")
+	router.HandleFunc("/api/rooms/{id}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Headers", "*")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		vars := mux.Vars(r)
+		roomID := vars["id"]
+		room, err := rooms.Get(roomID)
+		if err == lib.ErrNotFound {
+			http.NotFound(w, r)
+			return
+		}
+		bytes, err := json.Marshal(room.Wrap(nil))
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), 500)
+		}
+		w.Write(bytes)
+	}).Methods("GET")
+
+	router.HandleFunc("rooms/{id}", func(w http.ResponseWriter, r *http.Request) {
+		lib.ServeWs(rooms, w, r)
+	})
+	stop := make(chan bool)
+	videoBody := make(chan string, 3)
+	videoAns := make(chan string, 3)
+	router.HandleFunc("/establish-video-sdp", func(w http.ResponseWriter, r *http.Request) {
 		body, _ := ioutil.ReadAll(r.Body)
-		b <- string(body)
-		jsonData, _ := json.Marshal(&lib.WelcomeResponse{Message: <-ans})
+		videoBody <- string(body)
 		w.WriteHeader(200)
-		w.Write(jsonData)
+		w.Write([]byte(<-videoAns))
+	})
+
+	audioBody := make(chan string, 3)
+	audioAns := make(chan string, 3)
+	router.HandleFunc("/establish-audio-sdp", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		audioBody <- string(body)
+		w.WriteHeader(200)
+		w.Write([]byte(<-audioAns))
 	})
 
 	go func() {
-		http.ListenAndServe("localhost:50000", nil)
+		srv := &http.Server{
+			Handler:      router,
+			Addr:         "192.168.0.180:50000",
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+		}
+
+		srv.ListenAndServe()
 	}()
-	service.Worker(b, ans)
+	go service.VideoWorker(videoBody, videoAns)
+	go service.AudioWorker(audioBody, audioAns)
+	<-stop
 }
